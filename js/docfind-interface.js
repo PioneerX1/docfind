dd6fcd51352d23bf071554b768c0b50f7d920586
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-04 12:42:38 -0700

    add working api functionality, confirm can pull doctors first name, last name, title, and bio, and display

--- js/docfind-interface.js
+++ js/docfind-interface.js
@@ -1,14 +1,20 @@
 var DocFind = require('./../js/docfind.js').docFindModule;
 
+var getDoctorList = function(listing) {
+  var doctorName = listing.profile.first_name + " " + listing.profile.last_name + ", " + listing.profile.title;
+  var doctorBio = listing.profile.bio;
+  // $('#doc-name').append('<li>'+doctor+'</li>');
 
+  $('#doc-result').append('<div class="thumbnail">'+'<h4>'+doctorName+'</h4>'+'<p>'+doctorBio+'</p>'+'</div>');
+};
 
 
 //UI Form Input Logic
 $(document).ready(function() {
-  DocFind newDocFind = new DocFind(); //initialize object
+  var newDocFind = new DocFind(); //initialize object
   $('#symptom-form').submit(function(event) {
     event.preventDefault();
     var symptom = $('#input-symptom').val();
-
+    newDocFind.getDoctors(symptom, getDoctorList);
   });
 });
--- js/docfind.js
+++ js/docfind.js
@@ -4,7 +4,17 @@ function DocFind() {
 
 }
 
-DocFind.prototype.getDoctors = function(symptom)
+DocFind.prototype.getDoctors = function(symptom, getDoctorList) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+symptom+'&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key='+apiKey).then(function(response) {
+    response.data.forEach(function(listing) {
+      getDoctorList(listing);
+      console.log(JSON.stringify.response);
+  });
+  }).fail(function(error) {
+    console.log("Search failed");
+    // $('#fail').text(error.responseJSON.message);
+  });
+};
 
 
 
